6c2ebc4cbb5733c8894a0ff14532516626993359
Vincent Deloso <mitsugaru@gmail.com>
2013-03-15 17:25:05 -0600

    Check null on timesheet map.

--- src/com/amcgavin/playerlogger/LoggerPlugin.java
+++ src/com/amcgavin/playerlogger/LoggerPlugin.java
@@ -1,6 +1,5 @@
 package com.amcgavin.playerlogger;
 
-
 import java.util.Date;
 import java.util.HashMap;
 
@@ -8,39 +7,38 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoggerPlugin extends JavaPlugin {
 
-	private HashMap<String, Timesheet> loggingPlayers = new HashMap<String, Timesheet>();
-	public SQLDatabase database;
-	public LoggerConfig config;
-	public int defaultCap;
-	public void onDisable() {
-		System.out.print(this + " disabled.");	
-	}
+   private HashMap<String, Timesheet> loggingPlayers = new HashMap<String, Timesheet>();
+   public SQLDatabase database;
+   public LoggerConfig config;
+   public int defaultCap;
 
-	public void onEnable() {
-		System.out.print(this + " enabled.");
-		getServer().getPluginManager().registerEvents(new LogPlayerListener(this), this);
+   public void onEnable() {
+      getServer().getPluginManager().registerEvents(new LogPlayerListener(this), this);
 
-		config = new LoggerConfig();
-		config.load();
-		database = new SQLDatabase(this);
-	}
+      config = new LoggerConfig();
+      config.load();
+      database = new SQLDatabase(this);
+   }
 
-	public void addToLoggingPlayers(String player, int id, int base, long time) {
-		loggingPlayers.put(player, new Timesheet(id, base, time));
-	}
-	
-	public long removeFromLoggingPlayers(String player) {
-		Timesheet timesheet = loggingPlayers.get(player);
-		loggingPlayers.remove(player);
-		return ((new Date().getTime() - timesheet.getTime())/1000 + timesheet.getBase());
-	}
-	
-	public int getId(String player) {
-	   int id = -1;
-	   Timesheet ts = loggingPlayers.get(player);
-	   if(ts != null) {
-	      id = ts.getId();
-	   }
-		return id;
-	}
+   public void addToLoggingPlayers(String player, int id, int base, long time) {
+      loggingPlayers.put(player, new Timesheet(id, base, time));
+   }
+
+   public long removeFromLoggingPlayers(String player) {
+      long value = 0;
+      Timesheet timesheet = loggingPlayers.remove(player);
+      if(timesheet != null) {
+         value = ((new Date().getTime() - timesheet.getTime()) / 1000 + timesheet.getBase());
+      }
+      return value;
+   }
+
+   public int getId(String player) {
+      int id = -1;
+      Timesheet ts = loggingPlayers.get(player);
+      if(ts != null) {
+         id = ts.getId();
+      }
+      return id;
+   }
 }
